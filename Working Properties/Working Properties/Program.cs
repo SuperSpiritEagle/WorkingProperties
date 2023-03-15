3752462ed14650ec4ee1a9625f06f55246e17ace
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-03-15 17:27:38 +0500

    init

---
+++ Working Properties/Working Properties/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Working_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(3, 1);
+            Renderer renderer = new Renderer("@");
+
+            renderer.SetCursor(player.PositionX, player.PositionY);
+            renderer.Print();
+        }
+
+        class Player
+        {
+            public int PositionX { get;}
+            public int PositionY { get;}
+
+            public Player(int positionX,int positionY)
+            {
+                PositionX = positionX;
+                PositionY = positionY;
+            }
+        }
+
+        class Renderer
+        {
+            private string _player;
+            public Renderer(string player)
+            {
+                _player = player;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine(_player);
+            }
+
+            public void SetCursor(int positionX,int positionY)
+            {
+                Console.SetCursorPosition(positionX, positionY);
+            }
+        }
+    }
+}
+
+
